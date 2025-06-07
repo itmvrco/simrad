2de45610bbbcc692f34915b6955f1a6e6f15ad65
Marco <mmaarrccoo0129@gmail.com>
2025-06-07 13:04:14 -0400

    work on 64 bit machine, java23

--- src/at/innoc/roboat/radar/RadarFrameDecoder.java
+++ src/at/innoc/roboat/radar/RadarFrameDecoder.java
@@ -1,7 +1,7 @@
 package at.innoc.roboat.radar;
 
 import at.innoc.roboat.radar.RadarSource.RadarDataFrame;
-
+import org.jnetpcap.PcapClosedException;
 
 /**
  * Copyright 2011 by Adrian Dabrowski, Sebastian Busch
@@ -10,26 +10,25 @@ import at.innoc.roboat.radar.RadarSource.RadarDataFrame;
  * @author Adrian Dabrowski, Sebastian Busch
  *
  *
- * Decodes radar frames into single radar scan lines
+ *         Decodes radar frames into single radar scan lines
  *
  */
 public class RadarFrameDecoder implements Runnable {
 	Thread thread;
 	RadarSource source;
 	RadarLineListener handler;
-	
-	static final double SQRT2=Math.sqrt(2);
-	
+
+	static final double SQRT2 = Math.sqrt(2);
+
 	public RadarFrameDecoder(RadarSource s, RadarLineListener h) {
-		source=s;
-		handler=h;
-		thread=new Thread(this);
+		source = s;
+		handler = h;
+		thread = new Thread(this);
 		thread.setDaemon(true);
 		thread.start();
-		
+
 	}
-	
-	
+
 	/**
 	 * Suspend current thread, until RadarFrameDecoder received all frames.
 	 */
@@ -40,50 +39,50 @@ public class RadarFrameDecoder implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
 
 	@Override
 	public void run() {
-		boolean rawhandler=handler instanceof RadarRawLineListener;
-		RadarDataFrame dataFrame=null;
-		byte[] seqheader=new byte[8];
-		byte[] lineheader=new byte[24];
-		byte[] linedata=new byte[512];
-		int i=0;
-		
-		while ((dataFrame=source.getNextDataFrame()).getData()!=null) {
-			int len=dataFrame.getData().length;
-			System.arraycopy(dataFrame.getData(), 0, seqheader, 0, 8);
-			i=8;
-			while(len-i>=536) { // 536 == linedata(512) + lineheader(24)
-				System.arraycopy(dataFrame.getData(), i, lineheader, 0, 24);
-				System.arraycopy(dataFrame.getData(), i+24, linedata, 0, 512);
-				
-				int angleraw=(lineheader[9]&0xff)<<8 | (lineheader[8]&0xff);
-				float angle=((float)angleraw*360/4096) -90;
-				
-				//int rr=lineheader[23]&0xff; // rr
-				
-				int sstt=(lineheader[13]&0xff)<<8|(lineheader[12]&0xff);
-				
-				int meter;				
-				meter=(int)((double)sstt*10/SQRT2);
-				
-				if (rawhandler)
-					((RadarRawLineListener)handler).newRawRadarLine(lineheader, linedata);
-				
-				handler.newRadarLine(angle, meter, linedata);
-				
-				i+=536;
+		boolean rawhandler = handler instanceof RadarRawLineListener;
+		RadarDataFrame dataFrame = null;
+		byte[] seqheader = new byte[8];
+		byte[] lineheader = new byte[24];
+		byte[] linedata = new byte[512];
+		int i = 0;
+
+		try {
+			while ((dataFrame = source.getNextDataFrame()).getData() != null) {
+				int len = dataFrame.getData().length;
+				System.arraycopy(dataFrame.getData(), 0, seqheader, 0, 8);
+				i = 8;
+				while (len - i >= 536) { // 536 == linedata(512) + lineheader(24)
+					System.arraycopy(dataFrame.getData(), i, lineheader, 0, 24);
+					System.arraycopy(dataFrame.getData(), i + 24, linedata, 0, 512);
+
+					int angleraw = (lineheader[9] & 0xff) << 8 | (lineheader[8] & 0xff);
+					float angle = ((float) angleraw * 360 / 4096) - 90;
+
+					// int rr=lineheader[23]&0xff; // rr
+
+					int sstt = (lineheader[13] & 0xff) << 8 | (lineheader[12] & 0xff);
+
+					int meter;
+					meter = (int) ((double) sstt * 10 / SQRT2);
+
+					if (rawhandler)
+						((RadarRawLineListener) handler).newRawRadarLine(lineheader, linedata);
+
+					handler.newRadarLine(angle, meter, linedata);
+
+					i += 536;
+				}
+				handler.endOfFrame();
+
 			}
 			handler.endOfFrame();
-			
+			handler.onClose();
+		} catch (PcapClosedException e) {
+			System.out.println("PCAP file was closed by user.");
 		}
-		handler.endOfFrame();
-		handler.onClose();
 	}
 
-	
-	
-	
 }
